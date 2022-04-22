74e08b5bf969401cc5509efc5d106510764205bc
AldricVS <aldric.silvestre@outlook.fr>
2022-04-22 11:37:17 +0200

    Fix typo in dao

--- src/main/java/fr/nacvs/ied_mediator/dao/FilmPeopleDao.java
+++ src/main/java/fr/nacvs/ied_mediator/dao/FilmPeopleDao.java
@@ -9,5 +9,5 @@ public interface FilmPeopleDao {
 
 	Iterator<FilmPeople> findByActor(String actor);
 	
-	Optional<FilmPeople> findByTitleAndDistributor(String title, String distributor);
+	Optional<FilmPeople> findByTitleAndDirector(String title, String director);
 }
