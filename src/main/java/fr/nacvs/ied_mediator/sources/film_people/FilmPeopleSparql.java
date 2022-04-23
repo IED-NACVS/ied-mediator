2a0250abe121156ff839e9696a46406c50f7dc00
NicolasCIBULKA <nico.cibulka@gmail.com>
2022-04-23 12:03:28 +0200

    Implenting findByTitleAndDirector & refactoring a bit for my AldricUwU

--- src/main/java/fr/nacvs/ied_mediator/sources/film_people/FilmPeopleSparql.java
+++ src/main/java/fr/nacvs/ied_mediator/sources/film_people/FilmPeopleSparql.java
@@ -37,19 +37,7 @@ public class FilmPeopleSparql implements FilmPeopleDao {
     String serviceUrl = "https://dbpedia.org/sparql";
 
     // execute request
-    String queryString ="PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
-            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
-            "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" +
-            "PREFIX : <http://dbpedia.org/resource/>\n" +
-            "PREFIX dbpedia2: <http://dbpedia.org/property/>\n" +
-            "PREFIX dbpedia: <http://dbpedia.org/>\n" +
-            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
-            "PREFIX dbo: <http://dbpedia.org/ontology/>" +
-            "PREFIX dbr:<http://dbpedia.org/resource/>" +
-            "PREFIX dbp:<https://dbpedia.org/property/>"+
+    String queryString =getPrefix()+
             "SELECT \n" +
             "?real \n" +
             "?f \n" +
@@ -72,7 +60,7 @@ public class FilmPeopleSparql implements FilmPeopleDao {
 
     // create query output
     List<FilmPeople> outputList = new ArrayList<>();
-     //ResultSetFormatter.out(System.out, results, query);
+    //ResultSetFormatter.out(System.out, results, query);
 
     for ( ; results.hasNext() ; )
     {
@@ -101,7 +89,95 @@ public class FilmPeopleSparql implements FilmPeopleDao {
 
   @Override
   public Optional<FilmPeople> findByTitleAndDirector(String title, String director) {
-    return Optional.empty();
+    LOGGER.info("Calling findByTitleAndDirector : title :" + title + " -- director : " + director);
+    // create request
+    LOGGER.info( " creating request " );
+    Model model = ModelFactory.createDefaultModel() ;
+    String serviceUrl = "https://dbpedia.org/sparql";
+
+    // execute request
+    String queryString = getPrefix() +
+            "SELECT \n" +
+            "?real \n" +
+            "?f \n" +
+            "(GROUP_CONCAT( DISTINCT ?lp;separator=\"|\") AS ?prod)\n" +
+            "(GROUP_CONCAT( ?t;separator=\"|\") AS ?title) \n" +
+            "(GROUP_CONCAT( ?a;separator=\"|\") AS ?actors) \n" +
+            "WHERE {\n" +
+            "            ?f rdf:type dbo:Film.\n" +
+            "            ?f foaf:name \""+title+"\"@en.\n" +
+            "            ?f foaf:name ?t.\n"+
+
+            "            ?f dbo:starring ?as.\n"+
+            "            ?as foaf:name ?a.\n" +
+
+            "            ?f dbo:director ?ts.\n" +
+            "            ?ts foaf:name \""+director+"\"@en. \n" +
+
+            "            ?f dbo:director ?tr.\n"+
+            "            ?tr foaf:name ?real.\n" +
+
+            "            ?f dbo:producer ?p.\n" +
+            "            ?p foaf:name ?lp.\n" +
+            "} GROUP BY ?f ?real";
+    Query query = QueryFactory.create(queryString);
+    QueryExecution qe = QueryExecution.service(serviceUrl)
+            .query(queryString)
+            .build();
+    ResultSet results =  qe.execSelect();
+    // create query output
+    List<FilmPeople> outputList = new ArrayList<>();
+    //ResultSetFormatter.out(System.out, results, query);
+
+    for ( ; results.hasNext() ; )
+    {
+      QuerySolution soln = results.next() ;
+      String titre = Arrays.stream(soln.get("title").toString().split("\\|"))
+              .distinct()
+              .collect(Collectors.joining(""));
+      String real = soln.get("real").toString().replaceAll("@en", "");
+      String[] producers = soln.get("prod").toString().split("\\|");
+      String[] actors = soln.get("actors").toString().split("\\|");
+
+      FilmPeople fp = new FilmPeople();
+      fp.setTitle(titre);
+      fp.setDirector(real);
+      List<String> plist = new ArrayList<>();
+      for(String elt : producers){
+        plist.add(elt);
+      }
+      fp.setProducers(plist);
+
+      List<String> alist = new ArrayList<>();
+      for(String elt : actors){
+        alist.add(elt);
+      }
+      fp.setActors(alist);
+      outputList.add(fp);
+
+    }
+    qe.close();
+    LOGGER.info("End of findByActor ");
+    if (outputList.isEmpty()) {
+      return Optional.empty();
+    }
+    return Optional.of(outputList.get(0));
+  }
+
+  private String getPrefix(){
+    return "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+            "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" +
+            "PREFIX : <http://dbpedia.org/resource/>\n" +
+            "PREFIX dbpedia2: <http://dbpedia.org/property/>\n" +
+            "PREFIX dbpedia: <http://dbpedia.org/>\n" +
+            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+            "PREFIX dbo: <http://dbpedia.org/ontology/>" +
+            "PREFIX dbr:<http://dbpedia.org/resource/>" +
+            "PREFIX dbp:<https://dbpedia.org/property/>";
   }
 
 }
--- src/test/java/fr/nacvs/ied_mediator/manual/testsources.java
+++ src/test/java/fr/nacvs/ied_mediator/manual/testsources.java
@@ -1,11 +1,22 @@
 package fr.nacvs.ied_mediator.manual;
 
+import fr.nacvs.ied_mediator.business.FilmPeople;
 import fr.nacvs.ied_mediator.sources.film_people.FilmPeopleSparql;
+
+import java.util.Iterator;
+import java.util.Optional;
 
 public class testsources {
 
   public static void main(String[] args) {
+    System.out.println("--> Test on FindByActor");
     FilmPeopleSparql filmPeopleSparql = new FilmPeopleSparql();
-    filmPeopleSparql.findByActor("Johnny Depp");
+    Iterator<FilmPeople> itf = filmPeopleSparql.findByActor("Johnny Depp");
+    while(itf.hasNext()){
+      System.out.println(itf.next().toString());
+    }
+    System.out.println("--> Test on FindByNameAndDirector");
+    Optional<FilmPeople> fp = filmPeopleSparql.findByTitleAndDirector("Ed Wood", "Tim Burton");
+    System.out.println(fp.toString());
   }
 }
